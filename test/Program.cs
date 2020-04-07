b232a4282cabd0277752e9c301d42cd6b4c676ca
Fuad <fuad-022@mail.ru>
2020-04-07 21:49:51 +0400

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,89 @@
+﻿//Пространсво имен.
+using System;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        //комментарий
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+            //Console.WriteLine("Hello1");
+
+            //int a = 5;
+            //Console.WriteLine(a);
+
+            //string data;
+            //data = Console.ReadLine();
+            //Console.WriteLine(data);
+
+            //string str1 = "2.9";
+            //string str2 = "1";
+            //double a = Convert.ToDouble(str1);
+            //double a = double.Parse(str1);
+            //int a;
+            //bool result = int.TryParse(str2, out a);
+            //Console.WriteLine(result);
+
+            //Console.WriteLine("Hello world from test");
+
+            //bool isTrue = true;
+            //if (isTrue)
+            //{
+            //  Console.WriteLine("True");
+            //}
+
+            //ConsoleKey consoleKey = Console.ReadKey().Key;
+
+            //do
+            //{
+            //} while (true);
+
+            //for (; ; ) //безконечный цикл
+            //{
+            //    Console.WriteLine("for is working");
+            //    System.Threading.Thread.Sleep(300);
+            //    break; //код выйдет из цикла
+            //}
+
+            //int i = 0;
+            //for (; i < 3; i++)
+            //{
+            //    Console.WriteLine("for_1 " + i);
+            //}
+            //for (; i < 5; i++)
+            //{
+            //    Console.WriteLine("for_2 " + i);
+            //}
+
+            //for (int i = 0, j = 5; i < 10 && j < 12; i++, j++)
+            //{
+            //    Console.WriteLine("i - " + i);
+            //    Console.WriteLine("j - " + j);
+            //}
+
+            //int[] arr = Enumerable.Repeat(5, 10).ToArray();
+            //Console.ReadLine();
+
+            //int[] arr = { 10, 3, 2, 55 };
+            //foreach (int a in arr)
+            //{
+            //    Console.Write(a + " ");
+            //}
+            //Console.WriteLine(arr.Where(i => i % 2 == 0).Sum());
+            //Console.WriteLine(arr.Where(i => i % 2 != 0).Min());
+            //int[] result = arr.OrderByDescending(i => i).ToArray();
+            //Console.WriteLine(result);
+
+            //Index myIndex = ^1;
+            //Console.WriteLine(arr[myIndex]); //выводим с конца массива первый элемент(особенность C#8).
+            //int[] arr2 = arr[0..4]; //диапазон(особенность C#8).
+            //int[] arr3 = arr[..4]; //диапазон с начала (особенность C#8).
+            //int[] arr4 = arr[5..]; //диапазон с 5ти до конца (особенность C#8).
+            
+            
+        }
+    }
+}
